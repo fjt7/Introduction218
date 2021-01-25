450eb0da2ad49ea8b895a3e1481d7b7b78e7b425
fjt7 <fjt7@njit.edu>
2021-01-25 09:05:33 -0500

    Added "Web Applications" to line 9

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('IS 218 Here I Come!')
+    print_hi('IS 218 Here I Come! Web Applications')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
