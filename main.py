e6494dab9f869d781f8518fe3fad036ded64ae8e
fjt7 <fjt7@njit.edu>
2021-01-25 09:03:59 -0500

    Second Test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('IS 218')
+    print_hi('IS 218 Here I Come!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
